e8af39a195d2dc070be8427dca19a4c125cb0956
valeriew1 <pronina.lera2006@gmail.com>
2025-06-12 13:35:37 +0300

    Update ChooseObjectToMoveScript.cs

--- Assets/MyScripts/buttons/ChooseObjectToMoveScript.cs
+++ Assets/MyScripts/buttons/ChooseObjectToMoveScript.cs
@@ -3,33 +3,15 @@ using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class ChooseObjectToMoveScript : MonoBehaviour
-{ //при нажатии активирует фигуры по табу, тогда их можно перемещать и мб крутить
-
-    [Header("Настройки кнопки")]
+{
     [SerializeField] private GameObject[] AddObjects;
     [SerializeField] private GameObject centralObject;
     [SerializeField] private Button AddButt;
-    //[SerializeField] private Camera MainCamera;
-
     private bool activator = false;
     private int currentAddIndex = 0;
-    //private KeyCode selectionKey = KeyCode.Tab;
-    //private KeyCode addingKey = KeyCode.KeypadEnter;
-
     private GameObject chosenObj;
     private Color selectedColor = Color.darkGreen;
     private Material originMaterial;
     private Color originColor;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
