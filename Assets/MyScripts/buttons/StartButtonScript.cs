234f74ed5c6ad87fea599bae0fb0f1e7bc6a14ba
valeriew1 <pronina.lera2006@gmail.com>
2025-06-13 15:52:57 +0300

    Update StartButtonScript.cs

--- Assets/MyScripts/buttons/StartButtonScript.cs
+++ Assets/MyScripts/buttons/StartButtonScript.cs
@@ -1,93 +1,34 @@
 using System;
 using UnityEngine;
-using UnityEngine.UI; //нужно для работы addlistener
+using UnityEngine.UI; //Г­ГіГ¦Г­Г® Г¤Г«Гї Г°Г ГЎГ®ГІГ» addlistener
 
 public class StartButtonScript : MonoBehaviour
 {
-    //скрипт для кнопки start:
-    //запускает процесс падения мячика (включает гравитацию)
-    //либо позже изменить на: открывает opener (тогда можно не трогать гравитацию мячика)
-
-
     public Button startButt;
-
     GameObject ball;
-    //public GameObject Opener;
     public float GravityScale = 1f; 
-
-
-    //OpenerOpening opener = new OpenerOpening();
-
-    private Rigidbody2D ballRb; // Для 2D
+    private Rigidbody2D ballRb;
 
     void Start()
     {
-        //startButt = Button.FindObjectOfType(GameObject);
         ball = GameObject.FindGameObjectWithTag("Player");
         startButt.onClick.AddListener(OnStartButtonClick);
 
         if (ball != null)
         {
-            // Для 2D:
             ballRb = ball.GetComponent<Rigidbody2D>();
-
             if (ballRb != null)
             {
-                ballRb.gravityScale = 0f; // Выключаем гравитацию при старте
-                //ball.SetActive(false);
-                
+                ballRb.gravityScale = 0f; 
             }
         }
-        
-
-        //playerActivator = GetComponent< Rigidbody2D)>;
     }
 
     void OnStartButtonClick() 
     {
         if (ballRb != null)
         {
-            //ball.SetActive(true);
-            ballRb.gravityScale = GravityScale; // Включаем гравитацию
-            //Debug.Log("Гравитация активирована!");
-
-            //opener.OpenerMove();
-
-
+            ballRb.gravityScale = GravityScale; 
         }
-
-    }
-
-    
+    }    
 }
-
-// Метод для кнопки Start (вызывается через OnClick в инспекторе)
-//public void OnTrigger()
-//{
-//    if (ballRb != null)
-//    {
-//        ballRb.gravityScale = GravityScale; // Включаем гравитацию
-//        //Debug.Log("Гравитация активирована!");
-
-//        //opener.OpenerMove();
-
-//    }
-//    //else
-//    //{
-//    //    //Debug.LogError("Rigidbody не найден на объекте Ball!");
-//    //}
-//}
-
-//}
-//// Start is called once before the first execution of Update after the MonoBehaviour is created
-//void Start()
-//    {
-
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-
-//    }
-//}
