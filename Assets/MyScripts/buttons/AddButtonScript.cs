2eee70097731286f19a6fe67db6ff65026f6fa4e
valeriew1 <pronina.lera2006@gmail.com>
2025-06-13 16:43:10 +0300

    Update AddButtonScript.cs

--- Assets/MyScripts/buttons/AddButtonScript.cs
+++ Assets/MyScripts/buttons/AddButtonScript.cs
@@ -6,18 +6,17 @@ public class AddButtonScript : MonoBehaviour
     [SerializeField] private GameObject[] AddObjects;
     [SerializeField] private GameObject centralObject;
     [SerializeField] private Button AddButt;
-
     private GameObject chosenObj;
     private int currentAddIndex = 0;
     private bool activator = false;
     private Color selectedColor = Color.darkGreen;
     private Material originMaterial;
     private Color originColor;
+   
     private void Start()
     {
         AddButt.onClick.AddListener(OnAddButtonClick);
     }
-
     private void Update()
     {
         if (activator == true)
@@ -29,14 +28,12 @@ public class AddButtonScript : MonoBehaviour
             }
         }
     }
-
     private void AddObject() 
     {
         for (int i = 0; i < AddObjects.Length; i++)
         {
             currentAddIndex = i;
             chosenObj = AddObjects[currentAddIndex];
-            
             if (chosenObj != null)
             {
                 Vector3 vector3 = new Vector3(i-2, i-2, 0);
@@ -49,7 +46,6 @@ public class AddButtonScript : MonoBehaviour
             }
         }
     }
-
     private void OnAddButtonClick()
     {
         activator = true;
