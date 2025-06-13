bc12e8b6e7ecff04c4c7177eaf57f2f0a8fdb58d
valeriew1 <pronina.lera2006@gmail.com>
2025-06-13 16:48:05 +0300

    Update SceneManageLoadLevel.cs

--- Assets/MyScripts/buttons/SceneManageLoadLevel.cs
+++ Assets/MyScripts/buttons/SceneManageLoadLevel.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
 public class SceneManageLoadLevel : MonoBehaviour
 {
     [SerializeField] private Button newLevelBut;
@@ -10,7 +9,6 @@ public class SceneManageLoadLevel : MonoBehaviour
     {
         newLevelBut.onClick.AddListener(OnNextLevelButClick);
     }
-    
     public void OnNextLevelButClick() 
     {
         SceneManager.LoadScene(NextSceneOrder); 
