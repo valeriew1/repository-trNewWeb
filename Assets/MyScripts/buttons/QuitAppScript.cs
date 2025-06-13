2bbd24cb3598b4603dee73c1a3558055080ac87c
valeriew1 <pronina.lera2006@gmail.com>
2025-06-13 16:17:01 +0300

    Update NextLevelScript.cs

--- Assets/MyScripts/buttons/QuitAppScript.cs
+++ Assets/MyScripts/buttons/QuitAppScript.cs
@@ -4,18 +4,10 @@ using UnityEngine.UI;
 public class QuitAppScript : MonoBehaviour
 {
     [SerializeField] private Button QuitBut;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         QuitBut.onClick.AddListener(QuitButton);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void QuitButton() 
     {
         Application.Quit();
