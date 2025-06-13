5e3e0cf825deadbc3469e2a1ec85e1d3bb98b715
valeriew1 <pronina.lera2006@gmail.com>
2025-06-13 16:17:01 +0300

    Update NextLevelScript.cs

--- Assets/MyScripts/buttons/SceneManageLoadLevel.cs
+++ Assets/MyScripts/buttons/SceneManageLoadLevel.cs
@@ -6,25 +6,13 @@ public class SceneManageLoadLevel : MonoBehaviour
 {
     [SerializeField] private Button newLevelBut;
     [SerializeField] private int NextSceneOrder;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         newLevelBut.onClick.AddListener(OnNextLevelButClick);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void NextLevel() 
-    { 
-        //newLevelBut.onClick.AddListener(() => { SceneManager.LoadScene("Level1"); });
-
-
-    }
-
+    
     public void OnNextLevelButClick() 
-    { SceneManager.LoadScene(NextSceneOrder); }
+    {
+        SceneManager.LoadScene(NextSceneOrder); 
+    }
 }
