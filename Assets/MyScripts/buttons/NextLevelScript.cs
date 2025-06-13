999686e5e202093eb8d223ed04853b3620eeb5bc
valeriew1 <pronina.lera2006@gmail.com>
2025-06-13 16:17:01 +0300

    Update NextLevelScript.cs

--- Assets/MyScripts/buttons/NextLevelScript.cs
+++ Assets/MyScripts/buttons/NextLevelScript.cs
@@ -7,7 +7,7 @@ public class NextLevelScript : MonoBehaviour
 
     [SerializeField] private Button nextLevelButton;
     [SerializeField] private int NextLevelNumber;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         nextLevelButton.onClick.AddListener(OnNextLevelButtonClick);
