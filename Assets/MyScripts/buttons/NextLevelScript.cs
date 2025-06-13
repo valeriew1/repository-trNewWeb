e8cab304b4cf3f6dda0a634516796f8091d574bc
valeriew1 <pronina.lera2006@gmail.com>
2025-06-13 16:46:00 +0300

    Update NextLevelScript.cs

--- Assets/MyScripts/buttons/NextLevelScript.cs
+++ Assets/MyScripts/buttons/NextLevelScript.cs
@@ -1,17 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
 public class NextLevelScript : MonoBehaviour
 {
     [SerializeField] private Button nextLevelButton;
     [SerializeField] private int NextLevelNumber;
-    
     void Start()
     {
         nextLevelButton.onClick.AddListener(OnNextLevelButtonClick);
     }
-
     public void OnNextLevelButtonClick() 
     {
         SceneManager.LoadScene(NextLevelNumber);
