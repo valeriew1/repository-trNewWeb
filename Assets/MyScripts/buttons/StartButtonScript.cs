48a32c4027a2e5dc8e1f808f57fb77be7200df9f
valeriew1 <pronina.lera2006@gmail.com>
2025-06-13 16:48:50 +0300

    Update StartButtonScript.cs

--- Assets/MyScripts/buttons/StartButtonScript.cs
+++ Assets/MyScripts/buttons/StartButtonScript.cs
@@ -1,19 +1,16 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class StartButtonScript : MonoBehaviour
 {
     public Button startButt;
     GameObject ball;
     public float GravityScale = 1f; 
     private Rigidbody2D ballRb;
-
     void Start()
     {
         ball = GameObject.FindGameObjectWithTag("Player");
         startButt.onClick.AddListener(OnStartButtonClick);
-
         if (ball != null)
         {
             ballRb = ball.GetComponent<Rigidbody2D>();
@@ -23,7 +20,6 @@ public class StartButtonScript : MonoBehaviour
             }
         }
     }
-
     void OnStartButtonClick() 
     {
         if (ballRb != null)
